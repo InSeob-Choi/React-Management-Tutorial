1371ad6c326044ab15d7683dd965c6e38e481ff1
InSeob-Choi <dv3tp53fm@hanmail.net>
2022-08-04 16:54:52 +0900

    Creacte Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961112",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "정소철",
+    birthday: "901021",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "김희소",
+    birthday: "920208",
+    gender: "여자",
+    job: "회계사",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map((customer) => {
+        return (
+          <Customer
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        );
+      })}
     </div>
   );
 }
