d9f405602735349f70394bbf6f762a926a6b4314
InSeob-Choi <dv3tp53fm@hanmail.net>
2022-08-05 23:52:15 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
+import Paper from "@mui/material/Paper";
 import Customer from "./components/Customer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { styled } from "@mui/material/styles";
+
+const StylePaper = styled(Paper)(({ theme }) => ({
+  width: "100%",
+  marginTop: theme.spacing.uni * 3,
+  overflowX: "auto",
+}));
+const StyleTable = styled(Table)({
+  minWidth: 1080,
+});
 
 const customers = [
   {
@@ -30,21 +46,35 @@ const customers = [
 
 function App() {
   return (
-    <div>
-      {customers.map((customer) => {
-        return (
-          <Customer
-            key={customer.id}
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birthday}
-            gender={customer.gender}
-            job={customer.job}
-          />
-        );
-      })}
-    </div>
+    <StylePaper>
+      <StyleTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => {
+            return (
+              <Customer
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            );
+          })}
+        </TableBody>
+      </StyleTable>
+    </StylePaper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,19 @@
 import React from "react";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
 
 function Customer(props) {
   return (
-    <div>
-      <CustomerProfile id={props.id} image={props.image} name={props.name} />
-      <CustomerInfo
-        job={props.job}
-        birthday={props.birthday}
-        gender={props.gender}
-      />
-    </div>
-  );
-}
-
-function CustomerProfile(props) {
-  return (
-    <div>
-      <img src={props.image} alt="profile" />
-      <h2>
-        {props.name}({props.id})
-      </h2>
-    </div>
-  );
-}
-
-function CustomerInfo(props) {
-  return (
-    <div>
-      <p>{props.birthday}</p>
-      <p>{props.gender}</p>
-      <p>{props.job}</p>
-    </div>
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell>
+        <img src={props.image} alt="profile" />
+      </TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthday}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow>
   );
 }
 
